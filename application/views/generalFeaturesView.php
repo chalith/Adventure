d57a000d1b06a94d0a00c32459f415917663b701
chalith desaman <chalith desaman>
2016-12-02 17:45:15 +0530

    styles included

--- application/views/generalFeaturesView.php
+++ application/views/generalFeaturesView.php
@@ -10,23 +10,7 @@
     ?>
     <head>
         <title>FindYourRaft</title>
-        <!--
-        Conquer Template
-        http://www.templatemo.com/tm-476-conquer
-        -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" href="<?php echo base_url('css/bootstrap.min.css'); ?>">
-        <link rel="stylesheet" href="<?php echo base_url('css/home/style.css') ?>">
-        <link rel="stylesheet" href="<?php echo base_url('css/hovereffects.css') ?>">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-
-
-
+        <?php include 'styles.php'; ?>
     </head>
     <body>
         <?php include('header.php'); ?>   
--- application/views/home_view.php
+++ application/views/home_view.php
@@ -12,24 +12,8 @@
     ?>
     
 <head>
-        <title>FindYourRaft</title>
-	<meta charset="utf-8">
-        
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=ABeeZee:400,400italic">
-        
-	<link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="<?php echo base_url();?>css/home/login.css">
-	<link rel="stylesheet" href="<?php echo base_url();?>bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="<?php echo base_url();?>css/home/style.css">
-	
-        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-	<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-	<script src="jquery.json-2.4.min.js"></script>
-	<script src="http://code.jquery.com/jquery-latest.js"></script>
-	<script src="<?php echo base_url();?>bootstrap/js/bootstrap.min.js"></script>
+    <title>FindYourRaft</title>
+    <?php include 'styles.php'; ?>
 <script>
 $(document).ready(function(){
   var providers = $("#providersrow");
