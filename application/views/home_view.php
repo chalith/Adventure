b54b384c2541f9bd24902a24299a4992a1c4b7d9
chalith desaman <chalith desaman>
2016-12-11 13:31:14 +0530

    Features

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -325,28 +325,11 @@ $(document).ready(function(){
 					
 
 					<!-- Start Footer Area -->
-					<footer id="footer-area">
-						<div class="container">
-							<div class="row text-center">
-								<div class="col-sm-12">
-									<div class="footer-content">
-										<h1>FindYourAdventure</h1>
-										<p>“Find your adventure package”</p>
-										</div>                
-									</div>
-								</div>
-							</div>
-							<hr>
-							<div class="container">
-								<div class="row">
-									<div class="col-sm-12 text-center">             
-										<p class="copy">Copyright © 2084 Group 12 </p>
-									</div>
-								</div>
-							</div>
-						</footer>
-						<!-- End Footer Area -->
-					</div>
+                                        <?php include 'footer.php'; ?>
+                                        <!-- End Footer Area -->
+					
+					
+                </div>
 
 						<script src="js/jquery-1.11.2.min.js"></script>
 						<script src="js/jquery.scrollUp.min.js"></script> <!-- https://github.com/markgoodyear/scrollup -->
